f1a1b401f2f6405dbf27cac6e7932b9f4a506d0a
Subhayan <43519722+Subhayan18@users.noreply.github.com>
2021-03-25 10:14:58 +0100

    Delete CloneStrat directory

--- CloneStrat/R/pqn.R
+++
@@ -1,8 +0,0 @@
-pqn <- function (sample, ref = NULL){
-  S.sample = rowSums(abs(sample), na.rm = T)
-  sample = sample/S.sample
-  N.ref = apply(ref, 2, median, na.rm = T)
-  N.sample = t(t(sample)/N.ref)
-  param = apply(N.sample, 1, median, na.rm = T)
-  return(N.sample = sample/param)
-}
